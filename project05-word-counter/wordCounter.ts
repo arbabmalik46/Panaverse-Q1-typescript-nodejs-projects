1056cc79008ce9bb5b2dbcd66e8027496b8b5f1e
arbabmalik46 <arbabmalik46@gmail.com>
2023-06-21 19:01:57 +0500

    Word Counter Upgraded

--- project05-word-counter/wordCounter.ts
+++ project05-word-counter/wordCounter.ts
@@ -1,12 +1,22 @@
 import inquirer from "inquirer"
 
-console.log("Word Counter")
+async function name() {
+    
+}
+
+async function wordCounter(){
+    
+    console.log("Word Counter")
 let txt = await inquirer.prompt({
     name:"txt",
     type:"input",
     message:"Enter Text:"
 });
+    let txt1:string = txt.txt.toString();
+    const txtArray = txt1.split(" ");
+    console.log(`Total number of words in that line are: ${txtArray.length}`)
+}
 
-let txt1:string = txt.txt.toString();
-const txtArray = txt1.split(" ");
-console.log(`Total number of words in that line are: ${txtArray.length}`)
+wordCounter();
+
+
