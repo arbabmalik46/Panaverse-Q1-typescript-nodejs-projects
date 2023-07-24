9434a9665c8c528683cf35af3ce9965af9efb9b9
Arbab Malik <77314830+arbabmalik46@users.noreply.github.com>
2023-07-24 16:51:50 +0500

    Update quiz.ts

--- project08-quiz/quiz.ts
+++ project08-quiz/quiz.ts
@@ -1,3 +1,41 @@
-import chalk from "chalk";
 
-console.log(chalk.green("Quiz"))
+/*
+User will enter his credentials and system will verify if it is correct or not.
+ */
+console.log("Please Enter your credentials: ");
+
+let valid:boolean=false;
+
+let usernames = {
+    username:"abm",
+    password:2211
+}
+
+let usernamea =['abm','fa_1,','munawarr','babara1','sabirg','akhters','javeds','akhtara13','hussains13'];
+
+
+
+const checkusername = (username:string)=>{
+    var index_i = 0;
+    usernamea.forEach(element => {
+        if(element===username)
+        {
+            index_i = 1;
+        }
+    });
+    if(index_i === 1)
+    {
+        return true;
+    }
+    if(index_i === 0)
+    {
+        return false
+    }
+}
+if(checkusername("username"))
+{
+    valid=true;
+}
+else{
+    console.log("Please Check Username/Password");
+}
